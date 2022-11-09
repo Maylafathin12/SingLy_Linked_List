347c1fbdddc3424acb34a62c8051c79f6cf67bd3
TA1069 <maylafaat@gmail.com>
2022-11-09 16:46:35 +0700

    coba commit

--- SingLy_Linked_List/Program.cs
+++ SingLy_Linked_List/Program.cs
@@ -6,7 +6,7 @@ namespace SingLy_Linked_List
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");.
         }
     }
 }
